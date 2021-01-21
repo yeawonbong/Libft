985176c3978263197422af16fdf9fc37d2e94785
Yeawon Bong <ybong@c2r5s6.42seoul.kr>
2021-01-22 04:24:23 +0900

    backup libft

---
+++ ft_itoa.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybong <ybong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/09 21:59:07 by ybong             #+#    #+#             */
+/*   Updated: 2021/01/22 02:49:34 by ybong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_count_reslen(long long n)
+{
+	int			count;
+	int			minus;
+	int			reslen;
+
+	minus = 0;
+	if (n < 0)
+		minus = 1;
+	count = 1;
+	while (n / 10 != 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	reslen = count + minus;
+	return (reslen);
+}
+
+char		*ft_itoa(int n)
+{
+	long long	tempn;
+	char		*res;
+	int			reslen;
+	int			i;
+
+	i = 0;
+	tempn = (long long)n;
+	reslen = ft_count_reslen(tempn);
+	if (!(res = malloc(reslen + 1)))
+		return (0);
+	if (tempn < 0)
+	{
+		res[i++] = '-';
+		tempn *= -1;
+	}
+	res[reslen--] = '\0';
+	while (tempn / 10 != 0)
+	{
+		res[reslen] = (tempn % 10) + '0';
+		tempn = tempn / 10;
+		reslen--;
+	}
+	res[reslen] = tempn + '0';
+	return (res);
+}
